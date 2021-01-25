190641c1ef302b6a50acf1dc3768a504f005dfec
Driexus <72216504+Driexus@users.noreply.github.com>
2021-01-25 02:27:37 +0200

    Initial commit

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Map : MonoBehaviour
+{
+    private int tilemapIndex;
+    private Tilemap[] tilemaps;
+    private int tilemapCount;
+    public Player player;
+
+    public Tilemap ActiveTilemap
+    {
+        get
+        {
+            return tilemaps[tilemapIndex];
+        }
+    }
+
+    private void Awake()
+    {
+        tilemapCount = transform.childCount;
+        tilemaps = new Tilemap[tilemapCount];
+        
+        for (int i = 0; i < tilemapCount; i++)
+            tilemaps[i] = transform.GetChild(i).GetComponent<Tilemap>();
+    }
+
+    private void Start()
+    {
+        tilemaps[0].gameObject.SetActive(true);
+        tilemapIndex = 0;
+    }
+
+    public void NextTilemap()
+    {
+        tilemaps[tilemapIndex].gameObject.SetActive(false);
+        tilemapIndex++;
+        
+        if (tilemapIndex >= tilemapCount)
+            tilemapIndex = 0;
+
+        tilemaps[tilemapIndex].gameObject.SetActive(true);
+    }
+
+    public void PreviousTilemap()
+    {
+        tilemaps[tilemapIndex].gameObject.SetActive(false);
+        tilemapIndex--;
+
+        if (tilemapIndex < 0)
+            tilemapIndex = tilemapCount - 1;
+
+        tilemaps[tilemapIndex].gameObject.SetActive(true);
+    }
+
+    public void CheckFloor()
+    {
+        if (!player.HasFloor)
+            transform.parent.gameObject.GetComponent<Collider>().enabled = false;
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Player : MonoBehaviour
+{
+    public Map map;
+
+    public bool HasFloor
+    {
+        get
+        {
+            Vector3Int posToCheck = map.ActiveTilemap.WorldToCell(transform.position);
+            return map.ActiveTilemap.HasTile(posToCheck);
+        }
+    }
+}
