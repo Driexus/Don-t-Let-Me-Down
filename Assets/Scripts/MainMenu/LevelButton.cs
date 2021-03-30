36d96bb249663334b42b75064106b70cc0b3e11d
Driexus <72216504+Driexus@users.noreply.github.com>
2021-03-30 23:11:58 +0300

    Buttons art

--- Assets/Scripts/MainMenu/LevelButton.cs
+++ Assets/Scripts/MainMenu/LevelButton.cs
@@ -3,16 +3,20 @@ using UnityEngine.UI;
 using TMPro;
 
 [RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Image))]
 public class LevelButton : MonoBehaviour
 {
     public int level;
     public TMP_Text tmp_text;
+    public Sprite LockedLevel;
     Button button;
+    Image image;
     MainMenuManager MMM;
 
     private void Awake()
     {
         button = GetComponent<Button>();
+        image = GetComponent<Image>();
         MMM = GameObject.FindGameObjectWithTag("MainMenuManager").GetComponent<MainMenuManager>();
     }
 
@@ -21,7 +25,12 @@ public class LevelButton : MonoBehaviour
         button.onClick.AddListener(StartLevel);
         tmp_text.text = level.ToString();
         if (Saver.LoadData().level < level)
+        {
             button.interactable = false;
+            Destroy(tmp_text.gameObject);
+            image.sprite = LockedLevel;
+        }
+
     }
     public void StartLevel()
     {
