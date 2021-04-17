c668e723723481b42f9f80355213583c3f85243e
Driexus <72216504+Driexus@users.noreply.github.com>
2021-04-17 14:54:03 +0300

    Added Backgrounds

---
+++ Assets/Scenes/TimeTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTest : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float timescale;
+
+    private void OnEnable()
+    {
+        Time.timeScale = timescale;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1f;
+    }
+}
